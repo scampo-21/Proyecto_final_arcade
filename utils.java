a5b569888efa7edbc2509cb9c3eb323d8beff771
Samuel Campo <sdcampor@gmail.com>
2026-05-12 22:39:30 -0500

    primer

---
+++ utils.java
@@ -0,0 +1,38 @@
+public class utils {
+
+    public static void limpiarPantalla() {
+
+        for (int i = 0; i < 30; i++) {
+
+            System.out.println();
+        }
+    }
+
+    public static void titulo() {
+
+    System.out.println("  /$$$$$$  /$$        /$$$$$$  /$$$$$$$$");
+    System.out.println(" /$$__  $$| $$       /$$__  $$|__  $$__/");
+    System.out.println("| $$  \\__/| $$      | $$  \\ $$   | $$   ");
+    System.out.println("|  $$$$$$ | $$      | $$  | $$   | $$   ");
+    System.out.println(" \\____  $$| $$      | $$  | $$   | $$   ");
+    System.out.println(" /$$  \\ $$| $$      | $$  | $$   | $$   ");
+    System.out.println("|  $$$$$$/| $$$$$$$$|  $$$$$$/   | $$   ");
+    System.out.println(" \\______/ |________/ \\______/    |__/   ");
+    System.out.println();
+    System.out.println("        BIENVENIDO A LA SLOT MACHINE DEl BUFALO LUXE PALACE. ¡BUENA SUERTE!");
+}
+
+     public static void despedida() {
+
+    System.out.println("  /$$$$$$  /$$        /$$$$$$  /$$$$$$$$");
+    System.out.println(" /$$__  $$| $$       /$$__  $$|__  $$__/");
+    System.out.println("| $$  \\__/| $$      | $$  \\ $$   | $$   ");
+    System.out.println("|  $$$$$$ | $$      | $$  | $$   | $$   ");
+    System.out.println(" \\____  $$| $$      | $$  | $$   | $$   ");
+    System.out.println(" /$$  \\ $$| $$      | $$  | $$   | $$   ");
+    System.out.println("|  $$$$$$/| $$$$$$$$|  $$$$$$/   | $$   ");
+    System.out.println(" \\______/ |________/ \\______/    |__/   ");
+    System.out.println();
+    System.out.println("     ¡GRACIAS POR JUGAR! VUELVE PRONTO.");
+}
+}
